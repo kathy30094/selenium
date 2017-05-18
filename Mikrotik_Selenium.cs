c3ddae43f15d17355e5d10a0a7f51f89fcad99ca
kathy30094 <kathy30094@gmail.com>
2017-05-18 14:44:26 +0800

    selenium chrome driver

---
+++ Mikrotik_Selenium.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.IO;
+using System.Collections;
+
+
+namespace seleniumTest1
+{
+    static class Mikrotik_Selenium
+    {
+        public static ArrayList txtToList(string txtPlace)
+        {
+            StreamReader file = new StreamReader(txtPlace);  //要讀取的txt位置            
+            ArrayList list = new ArrayList();
+            string lineInTxt;
+            //readline並依序放入list中
+            while ((lineInTxt = file.ReadLine()) != null)
+            {
+                list.Add(lineInTxt);
+            }
+            return list;
+        }//EX:txtPlace= "g:\\all_IP.txt";
+
+        public static void loginListOfMikrotik(ArrayList IPlist, string name, string password)
+        {
+            //開啟chrome
+            IWebDriver driver = new ChromeDriver();
+
+            foreach (string Line in IPlist)
+            {
+                driver.Navigate().GoToUrl("http://" + Line);
+
+                //login page-->找尋相對映的element填入值
+                IWebElement elementName = driver.FindElement(By.Id("name"));
+                elementName.Clear();//清除預設值admin
+                elementName.SendKeys(name);
+
+                IWebElement elementPW = driver.FindElement(By.Id("password"));
+                elementPW.SendKeys(password + Keys.Enter);  //輸入完後直接Enter
+
+                Console.ReadLine();//中斷
+            }
+        }//依序登入列表中Mikrotik的IP
+
+        public static void MikrotikLogin(IWebDriver driver,string IP, string name, string password)
+        {
+            //連入網址            
+            driver.Navigate().GoToUrl("http://" + IP);
+
+            //login page-->找尋相對映的element填入值
+            IWebElement elementName = driver.FindElement(By.Id("name"));
+            elementName.Clear();//清除預設值admin
+            elementName.SendKeys(name);
+
+            IWebElement elementPW = driver.FindElement(By.Id("password"));
+            elementPW.SendKeys(password + Keys.Enter);  //輸入完後直接Enter
+        }
+
+        public static void checkListOfMikrotik(ArrayList IPlist)
+        {
+            //開啟chrome
+            IWebDriver driver = new ChromeDriver();
+
+            //取出list中IP依序連入
+            foreach (string Line in IPlist)
+            {
+                //連入網址
+                driver.Navigate().GoToUrl("http://" + Line);
+                Console.ReadLine();//中斷
+            }
+
+
+        }
+
+       
+    }
+}
---
+++ Selenium.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.IO;
+using System.Collections;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Collections.ObjectModel;
+namespace seleniumTest1
+{
+    class Selenium
+    {
+        public static void switchWindow(IWebDriver driver)
+        {
+            Thread.Sleep(3000);
+            var handles = driver.WindowHandles;
+            //尋覽至最後一個
+            foreach (var handle in handles)
+            {
+                driver.SwitchTo().Window(handle);
+            }
+        }//切換至另一視窗
+
+        public static void implicitWait(IWebDriver driver)
+        {
+            TimeSpan timeout = TimeSpan.FromMilliseconds(3000);//1sec
+            driver.Manage().Timeouts().ImplicitWait = timeout;
+        }//整個程式的預設等待，只要有任何東西找不到就自動等待
+
+        public static void explicitWait1sec(IWebDriver driver, string element, string elementType)
+        {
+            //教學https://www.youtube.com/watch?v=0eSORVemv80&list=PL6tu16kXT9PoIHrwaqkBitAmjlrG1Bgxp&index=3
+            //the timeout to wait
+            TimeSpan timeout = TimeSpan.FromMilliseconds(1000);//1sec
+            WebDriverWait wait = new WebDriverWait(driver, timeout);
+
+            if (elementType == "Id")
+            {
+                wait.Until(ExpectedConditions
+                .ElementToBeClickable(By.Id(element)));
+            }
+            if (elementType == "Name")
+            {
+                wait.Until(ExpectedConditions
+                   .ElementToBeClickable(By.Name(element)));
+            }
+            if (elementType == "LinkText")
+            {
+                wait.Until(ExpectedConditions
+                .ElementToBeClickable(By.LinkText(element)));
+            }
+            if (elementType == "ClassName")
+            {
+                wait.Until(ExpectedConditions
+                .ElementToBeClickable(By.ClassName(element)));
+            }
+            if (elementType == "XPath")
+            {
+                wait.Until(ExpectedConditions
+                .ElementToBeClickable(By.XPath(element)));
+            }
+            else
+            {
+                Console.WriteLine("Element Type not include !");
+            }
+        }//Id, XPath, ClassName, LinkText, Name
+
+        public static void clickElement(IWebDriver driver, string element, string elementType)
+        {
+            if (elementType == "Id")
+            {
+                IWebElement menuTerminal = driver.FindElement(By.Id(element));
+                menuTerminal.Click();
+            }
+            if (elementType == "Name")
+            {
+                IWebElement menuTerminal = driver.FindElement(By.Name(element));
+                menuTerminal.Click();
+            }
+            if (elementType == "LinkText")
+            {
+                IWebElement menuTerminal = driver.FindElement(By.LinkText(element));
+                menuTerminal.Click();
+            }
+            if (elementType == "ClassName")
+            {
+                IWebElement menuTerminal = driver.FindElement(By.ClassName(element));
+                menuTerminal.Click();
+            }
+            if (elementType == "XPath")
+            {
+                IWebElement menuTerminal = driver.FindElement(By.XPath(element));
+                menuTerminal.Click();
+            }
+        }//Id, XPath, ClassName, LinkText, Name
+    }
+}
